f7e486f4d78b647a56f264671482c1e5d4703b54
Akshat-881236 <akshatpsd2005@gmail.com>
2026-07-08 21:09:44 +0530

    Update API Config and remove secrets

--- public/config.js
+++ public/config.js
@@ -6,4 +6,4 @@
 // Do NOT include a trailing slash (e.g. use "https://dpg-notes-backend.onrender.com" instead of "https://dpg-notes-backend.onrender.com/")
 // If you are testing locally, you can change this to "http://localhost:5000"
 
-window.API_BASE_URL = "https://your-render-api-url.onrender.com";
+window.API_BASE_URL = "https://dpgnotes.onrender.com";
